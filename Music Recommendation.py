bc5c8f029de2b3b2fec6a196b50af8e4e3d4e90b
HishamElkholy <136210786+HishamElkholy@users.noreply.github.com>
2023-10-05 22:45:02 -0400

    Add files via upload

---
+++ Music Recommendation.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+	page_title="Title Page",
+	page_icon="(m)"
+)
+
+
+
+st.title("Music Recommendation")
+st.image("MusRec.png")
+st.write("Liliana L. Cubas Gaona")
+st.write("Jorge Medina Victoria")
+st.write("Hisham Elkholy")
