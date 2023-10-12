13f4d0a40fd6df8c9743c2dd5bf07a7e193150f2
HishamElkholy <136210786+HishamElkholy@users.noreply.github.com>
2023-10-11 20:51:38 -0400

    Update Music_Recommendation.py

--- Music_Recommendation.py
+++ Music_Recommendation.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 st.title("Music Recommendation")
-#st.image("MusRec.png")
+st.image("MusRec.png")
 st.write("Liliana L. Cubas Gaona")
 st.write("Jorge Medina Victoria")
 st.write("Hisham Elkholy")
