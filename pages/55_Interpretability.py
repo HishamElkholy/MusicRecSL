d813dfdfd28799c9bea57bf1fdb24c249aab92d9
HishamElkholy <136210786+HishamElkholy@users.noreply.github.com>
2023-10-23 21:05:57 -0400

    Update 55_Interpretability.py

--- pages/55_Interpretability.py
+++ pages/55_Interpretability.py
@@ -13,3 +13,6 @@ st.title("SHAP Analysis")
 """- Among the content features, "language English” and “tz_North America & South America”' stand out as the most crucial contributor to music appreciation predictions when observing the plot of the absolute mean SHAP values.The figure displays the features which made a significant contribution to the model predictions.It is important to note that the majority of entries are for the English language and for the North America & South America time zone. This may lead to the importance of these features for the calculation of the model."""
 
 st.image("SHAP1.png", caption = "Bar plot using the SHAP values for a specific instance. Absolute mean contribution of the features")
+
+"""- Languages, time zones and musical features: The SHAP Values of the Waterfall plot in Figure 21 describe how every model feature contributes to the difference between the prediction f(x) = 1.389 and the average prediction E[f(x] = -1.039. Besides this number it is important to state that the red values (positive) describe a higher probability to predict a positive sentiment score whereas the blue values (negative) increase the probability to predict a negative sentiment score."""
+
